cdc968d4ff00fb5b4adc0c7a5e10e7253dc66ecf
lemo <lemo@gamstudio.net>
2021-07-01 23:07:58 +0800

    1.init.

--- main.go
+++ main.go
@@ -12,15 +12,16 @@ package caller
 
 import (
 	"os"
+	"reflect"
 	"runtime"
+	"runtime/debug"
+	"strconv"
 	"strings"
 )
 
 var rootPath, _ = os.Getwd()
 
-var packageName = "github.com/lemoyxk/caller"
-
-func Auto() (string, int) {
+func Auto(packageName string) (string, int) {
 
 	var file, line = "", 0
 
@@ -38,23 +39,7 @@ func Auto() (string, int) {
 		}
 	}
 
-	if file == "" || line == 0 {
-		return "", 0
-	}
-
-	if runtime.GOOS == "windows" {
-		rootPath = strings.Replace(rootPath, "\\", "/", -1)
-	}
-
-	if rootPath == "/" {
-		return file, line
-	}
-
-	if strings.HasPrefix(file, rootPath) {
-		file = file[len(rootPath)+1:]
-	}
-
-	return file, line
+	return clipFileAndLine(file, line)
 }
 
 func Deep(deep int) (string, int) {
@@ -67,6 +52,32 @@ func Deep(deep int) (string, int) {
 
 	file, line = codePath, codeLine
 
+	return clipFileAndLine(file, line)
+}
+
+func Stack(deep int) (string, int) {
+	var list = strings.Split(string(debug.Stack()), "\n")
+	var info = strings.TrimSpace(list[deep])
+	var flInfo = strings.Split(strings.Split(info, " ")[0], ":")
+	var file, l = flInfo[0], flInfo[1]
+	var line, _ = strconv.Atoi(l)
+	return clipFileAndLine(file, line)
+}
+
+func GetFuncName(fn interface{}) string {
+	t := reflect.ValueOf(fn).Type()
+	if t.Kind() == reflect.Func {
+		return runtime.FuncForPC(reflect.ValueOf(fn).Pointer()).Name()
+	}
+	return t.String()
+}
+
+func FuncName() string {
+	pc, _, _, _ := runtime.Caller(1)
+	return runtime.FuncForPC(pc).Name()
+}
+
+func clipFileAndLine(file string, line int) (string, int) {
 	if file == "" || line == 0 {
 		return "", 0
 	}
